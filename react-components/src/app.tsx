78c211623b6e082f3522f1f25e8eb3e1da284e53
WFZ1 <pavelskumenko@gmail.com>
2021-08-02 16:30:01 +0300

    init: start react-components task

---
+++ react-components/src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render(): JSX.Element {
+    return (
+      <div className="app">
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
