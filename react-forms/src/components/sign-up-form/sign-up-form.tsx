b5af0e866df823b0a660b9c51906217e5091ba81
WFZ1 <pavelskumenko@gmail.com>
2021-08-09 10:29:08 +0300

    feat: add fields validation

--- react-forms/src/components/sign-up-form/sign-up-form.tsx
+++ react-forms/src/components/sign-up-form/sign-up-form.tsx
@@ -1,54 +1,161 @@
 import './sign-up-form.scss';
 import '@fortawesome/fontawesome-free/css/all.css';
 import React from 'react';
+import ISignUpFormState from '../../types/sign-up-form-state.type';
+import ISignUpFormFieldsValues from '../../types/sign-up-form-fields-values.type';
+import { SIGN_UP_FORM_FIELDS } from '../../constants';
 
-export default class SignUpForm extends React.Component<{ classes: string }> {
-  // private handleSunbmit(e: Event) {
-  //   e.preventDefault();
-  // }
+export default class SignUpForm extends React.Component<{ classes: string }, ISignUpFormState> {
+  constructor(props: { classes: string }) {
+    super(props);
 
-  // onSubmit={ this.handleSubmit }
+    this.state = {
+      fields: {
+        fullName: '',
+        email: '',
+        password: '',
+        dateOfBirth: '',
+        gender: '',
+        paymentMethod: 'card',
+        cardNumber: '',
+        cardCvv: '',
+        cardExpireMonth: 'pick-month',
+        cardExpireYear: 'pick-year',
+        termsAndConditions: false,
+        marketingInfo: true,
+      },
+      errors: []
+    };
+  }
+
+  private handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    const keys = Object.keys(this.state.fields);
+    let errors: string[] = [];
+
+    keys.forEach((key) => {
+      const isErrorsArr = this.checkValidation(key as keyof ISignUpFormFieldsValues);
+
+      if (isErrorsArr) {
+        errors = errors.concat(isErrorsArr);
+      }
+    });
+
+    if (errors.length) {
+      this.setState((prevState) => ({
+        ...prevState,
+        errors
+      }));
+    }
+  }
+
+  private handleChange(e: React.FormEvent): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState((prevState) => ({
+      ...prevState,
+      fields: {
+        ...prevState.fields,
+        [target.name]: value
+      }
+    }));
+  }
+
+  private checkValidation(fieldName: keyof ISignUpFormFieldsValues): string[] | false {
+    const field = SIGN_UP_FORM_FIELDS.find((fld) => fld.name === fieldName);
+
+    if (!field) return false;
+
+    const fieldValue = String(this.state.fields[fieldName]);
+    let errors = [
+      ...this.state.errors.filter(fld => fld !== fieldName),
+    ];
+
+    if (!field.regex.test(fieldValue)) {
+      errors = [...errors, fieldName];
+    }
+
+    return errors;
+  }
+
+  private defineChangedField(prevState: ISignUpFormState): void {
+    const fields = Object.keys(this.state.fields);
+
+    fields.forEach((field) => {
+      const key = field as keyof ISignUpFormFieldsValues;
+
+      if (this.state.fields[key] !== prevState.fields[key]) {
+        const errors = this.checkValidation(key);
+        if (!errors) return;
+
+        this.setState((previousState) => ({
+          ...previousState,
+          errors
+        }));
+      }
+    });
+  }
+
+  componentDidUpdate(prevProps: { classes: string }, prevState: ISignUpFormState): void {
+    this.defineChangedField(prevState);
+  }
 
   render(): JSX.Element {
     return (
-      <form className={`sign-up-form ${this.props.classes}`}>
+      <form className={`sign-up-form ${this.props.classes}`} noValidate onSubmit={ (e) => this.handleSubmit(e) }>
         <section className="sign-up-form__section">
           <h4 className="sign-up-form__subtitle">Account</h4>
 
-          <div className="sign-up-form__input-group sign-up-form__input-group-icon">
+          <div className={ `sign-up-form__input-group sign-up-form__input-group-icon ${ this.state.errors.includes('fullName') ? 'sign-up-form__input__invalid' : '' }` }>
             <input
               className="sign-up-form__input"
               type="text"
-              name="full-name"
+              value={ this.state.fields.fullName }
+              name="fullName"
               placeholder="Full Name"
+              required
+              onChange={ (e) => this.handleChange(e) }
             />
             <div className="sign-up-form__input-icon">
               <i className="fa fa-user"></i>
             </div>
+            <p className="sign-up-form__input-error">
+              { 'Full name can\'t be empty, contain only numbers, contain service characters (~ ! @ # $ % * () _ — + = | : ; " \' ` < > , . ? / ^).' }
+            </p>
           </div>
 
-          <div className="sign-up-form__input-group sign-up-form__input-group-icon">
+          <div className={ `sign-up-form__input-group sign-up-form__input-group-icon ${ this.state.errors.includes('email') ? 'sign-up-form__input__invalid' : '' }` }>
             <input
               className="sign-up-form__input"
               type="email"
+              value={ this.state.fields.email }
               name="email"
               placeholder="Email Adress"
+              required
+              onChange={ (e) => this.handleChange(e) }
             />
             <div className="sign-up-form__input-icon">
               <i className="fa fa-envelope"></i>
             </div>
+            <p className="sign-up-form__input-error">Email can&rsquo;t be empty, must comply with the standard email generation rule RFC</p>
           </div>
 
-          <div className="sign-up-form__input-group sign-up-form__input-group-icon">
+          <div className={ `sign-up-form__input-group sign-up-form__input-group-icon ${ this.state.errors.includes('password') ? 'sign-up-form__input__invalid' : '' }` }>
             <input
               className="sign-up-form__input"
               type="password"
+              value={ this.state.fields.password }
               name="password"
               placeholder="Password"
+              required
+              onChange={ (e) => this.handleChange(e) }
             />
             <div className="sign-up-form__input-icon">
               <i className="fa fa-key"></i>
             </div>
+            <p className="sign-up-form__input-error">Password must have length from 6 to 20 characters, contain numbers, small and capital letters.</p>
           </div>
         </section>
 
@@ -56,27 +163,32 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
           <div className="sign-up-form__col-half">
             <h4 className="sign-up-form__subtitle">Date of Birth</h4>
 
-            <div className="sign-up-form__input-group">
+            <div className={ `sign-up-form__input-group ${ this.state.errors.includes('dateOfBirth') ? 'sign-up-form__input__invalid' : '' }` }>
               <input
                 className="sign-up-form__input sign-up-form__input-date"
                 type="date"
-                name="date-of-birth"
+                value={ this.state.fields.dateOfBirth }
+                name="dateOfBirth"
                 min="1900-01-01"
                 max="2021-12-31"
+                onChange={ (e) => this.handleChange(e) }
               />
+              <p className="sign-up-form__input-error">Date of birth must set.</p>
             </div>
           </div>
 
           <div className="sign-up-form__col-half">
             <h4 className="sign-up-form__subtitle">Gender</h4>
 
-            <div className="sign-up-form__input-group">
+            <div className={ `sign-up-form__input-group ${ this.state.errors.includes('gender') ? 'sign-up-form__input__invalid' : '' }` }>
               <input
                 className="sign-up-form__input sign-up-form__input-radio"
                 id="sign-up-form__gender-male"
                 type="radio"
+                checked={ this.state.fields.gender === 'male' }
                 name="gender"
                 value="male"
+                onChange={ (e) => this.handleChange(e) }
               />
               <label
                 className="sign-up-form__label"
@@ -89,8 +201,10 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
                 className="sign-up-form__input sign-up-form__input-radio"
                 id="sign-up-form__gender-female"
                 type="radio"
+                checked={ this.state.fields.gender === 'female' }
                 name="gender"
                 value="female"
+                onChange={ (e) => this.handleChange(e) }
               />
               <label
                 className="sign-up-form__label"
@@ -98,6 +212,8 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
               >
                 Female
               </label>
+
+              <p className="sign-up-form__input-error">Gender must select.</p>
             </div>
           </div>
         </section>
@@ -110,9 +226,10 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
               className="sign-up-form__input sign-up-form__input-radio"
               id="sign-up-form__payment-method-card"
               type="radio"
-              name="payment-method"
+              checked={ this.state.fields.paymentMethod === 'card' }
+              name="paymentMethod"
               value="card"
-              checked
+              onChange={ (e) => this.handleChange(e) }
             />
             <label
               className="sign-up-form__label"
@@ -125,8 +242,11 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
               className="sign-up-form__input sign-up-form__input-radio"
               id="sign-up-form__payment-method-paypal"
               type="radio"
-              name="payment-method"
+              checked={ this.state.fields.paymentMethod === 'paypal' }
+              name="paymentMethod"
               value="paypal"
+              onChange={ (e) => this.handleChange(e) }
+              disabled
             />
             <label
               className="sign-up-form__label"
@@ -136,39 +256,52 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
             </label>
           </div>
 
-          <div className="sign-up-form__input-group sign-up-form__input-group-icon">
+          <div className={ `sign-up-form__input-group sign-up-form__input-group-icon ${ this.state.errors.includes('cardNumber') ? 'sign-up-form__input__invalid' : '' }` }>
             <input
               className="sign-up-form__input"
               type="text"
-              name="card-number"
+              value={ this.state.fields.cardNumber }
+              name="cardNumber"
               placeholder="Card Number"
+              maxLength={ 16 }
+              onChange={ (e) => this.handleChange(e) }
             />
             <div className="sign-up-form__input-icon">
               <i className="fa fa-credit-card"></i>
             </div>
+            <p className="sign-up-form__input-error">Card number must have 16 numbers.</p>
           </div>
 
           <div className="sign-up-form__col-half">
-            <div className="sign-up-form__input-group sign-up-form__input-group-icon">
+            <div className={ `sign-up-form__input-group sign-up-form__input-group-icon ${ this.state.errors.includes('cardCvv') ? 'sign-up-form__input__invalid' : '' }` }>
               <input
                 className="sign-up-form__input"
                 type="text"
-                name="card-cvv"
+                value={ this.state.fields.cardCvv }
+                name="cardCvv"
                 placeholder="Card CVV"
                 maxLength={ 3 }
+                onChange={ (e) => this.handleChange(e) }
               />
               <div className="sign-up-form__input-icon">
                 <i className="fa fa-user"></i>
               </div>
+              <p className="sign-up-form__input-error">Card CVV must have 3 numbers.</p>
             </div>
           </div>
 
           <div className="sign-up-form__col-half">
-            <div className="sign-up-form__input-group">
+            <div className={ `sign-up-form__input-group ${ (this.state.errors.includes('cardExpireMonth') || this.state.errors.includes('cardExpireYear')) ? 'sign-up-form__input__invalid' : '' }` }>
               <select
                 className="sign-up-form__select sign-up-form__card-expire-month"
-                name="card-expire-month"
+                value={ this.state.fields.cardExpireMonth }
+                name="cardExpireMonth"
+                onChange={ (e) => this.handleChange(e) }
+                onBlur={ (e) => this.handleChange(e) }
               >
+                <option className="sign-up-form__option" value="pick-month">
+                  Pick month
+                </option>
                 <option className="sign-up-form__option" value="01">
                   01
                 </option>
@@ -208,8 +341,14 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
               </select>
               <select
                 className="sign-up-form__select sign-up-form__card-expire-year"
-                name="card-expire-year"
+                value={ this.state.fields.cardExpireYear }
+                name="cardExpireYear"
+                onChange={ (e) => this.handleChange(e) }
+                onBlur={ (e) => this.handleChange(e) }
               >
+                <option className="sign-up-form__option" value="pick-year">
+                  Pick year
+                </option>
                 <option className="sign-up-form__option" value="2021">
                   2021
                 </option>
@@ -241,6 +380,8 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
                   2030
                 </option>
               </select>
+
+              <p className="sign-up-form__input-error">Card expiry date must set.</p>
             </div>
           </div>
         </section>
@@ -248,11 +389,15 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
         <section className="sign-up-form__section sign-up-form__terms">
           <h4 className="sign-up-form__subtitle">Terms and Conditions</h4>
 
-          <div className="sign-up-form__input-group">
+          <div className={ `sign-up-form__input-group ${ this.state.errors.includes('termsAndConditions') ? 'sign-up-form__input__invalid' : '' }` }>
             <input
               className="sign-up-form__input sign-up-form__input-checkbox"
               id="sign-up-form__terms"
+              checked={ this.state.fields.termsAndConditions }
               type="checkbox"
+              name="termsAndConditions"
+              required
+              onChange={ (e) => this.handleChange(e) }
             />
             <label
               className="sign-up-form__label"
@@ -261,21 +406,31 @@ export default class SignUpForm extends React.Component<{ classes: string }> {
               I accept the terms and conditions for signing up to this service,
               and hereby confirm I have read the privacy policy.
             </label>
+            <p className="sign-up-form__input-error">Please accept our terms & conditions.</p>
           </div>
         </section>
 
         <section className="sign-up-form__section">
           <h4 className="sign-up-form__subtitle">Email Notifications</h4>
 
-          <div className="sign-up-form__input-group sign-up-form__switcher-container">
+          <div className={ `sign-up-form__input-group sign-up-form__switcher-container ${ this.state.errors.includes('marketingInfo') ? 'sign-up-form__input__invalid' : '' }` }>
             <label className="sign-up-form__switcher sign-up-form__notifications-switcher">
-              <input className="sign-up-form__switcher-checkbox" type="checkbox" />
+              <input
+                className="sign-up-form__switcher-checkbox"
+                type="checkbox"
+                checked={ this.state.fields.marketingInfo }
+                name="marketingInfo"
+                onChange={ (e) => this.handleChange(e) }
+              />
               <span className="sign-up-form__switcher-panel" data-on="On" data-off="Off"></span>
               <span className="sign-up-form__switcher-handle"></span>
             </label>
             <p className="sign-up-form__switcher-text">Marketing information, offers, updates, promotions, including current or future product announcements and updates.</p>
+            <p className="sign-up-form__input-error">Please enable email notifications.</p>
           </div>
         </section>
+
+        <input className="sign-up-form__btn-submit" type="submit" value="Submit" />
       </form>
     );
   }
