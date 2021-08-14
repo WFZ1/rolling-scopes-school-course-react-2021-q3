4819759703b682b8e5e15f26e063aa4d8501b31d
WFZ1 <pavelskumenko@gmail.com>
2021-08-14 09:52:07 +0300

    feat: add animation a page route transition

--- react-routing/src/app.tsx
+++ react-routing/src/app.tsx
@@ -5,45 +5,57 @@ import {
   Route,
   NavLink
 } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import HomePage from './components/home-page/home-page';
 import AboutPage from './components/about-page/about-page';
 import NotFoundPage from './components/not-found-page/not-found-page';
 import DetailsPage from './components/details-page/details-page';
 
+const ROUTES = [
+  { path: '/', name: 'Home', Component: HomePage },
+  { path: '/about', name: 'About', Component: AboutPage },
+  { path: '/details', Component: DetailsPage },
+  { path: '*', name: '404', Component: NotFoundPage }
+]
+
 export default class App extends React.Component {
   render(): JSX.Element {
     return (
       <Router>
-        <header className="header">
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item">
-                <NavLink to="/" exact className="header__nav-link" activeClassName="header__nav-link_active">Home</NavLink>
-              </li>
-              <li className="header__nav-item">
-                <NavLink to="/about" exact className="header__nav-link" activeClassName="header__nav-link_active">About</NavLink>
-              </li>
-              <li className="header__nav-item">
-                <NavLink to="/not-found" exact className="header__nav-link" activeClassName="header__nav-link_active">404</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Route render={({ location }) => (
+          <>
+            <header className="header">
+              <nav className="header__nav">
+                <ul className="header__nav-list">
+                  {
+                    ROUTES.map(route => (
+                      route.name ?
+                        <li className="header__nav-item" key={ route.path }>
+                          <NavLink to={ route.path } exact className="header__nav-link" activeClassName="header__nav-link_active">{ route.name }</NavLink>
+                        </li> : null
+                    ))
+                  }
+                </ul>
+              </nav>
+            </header>
 
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/about">
-            <AboutPage />
-          </Route>
-          <Route path="/details">
-            <DetailsPage />
-          </Route>
-          <Route path="*">
-            <NotFoundPage />
-          </Route>
-        </Switch>
+            <TransitionGroup>
+              <CSSTransition
+                key={ location.key }
+                classNames="main"
+                timeout={ 300 }
+              >
+                <Switch location={ location }>
+                  {
+                    ROUTES.map(({ path, Component }) => (
+                      <Route key={ path } exact path={ path } component={ Component } />
+                    ))
+                  }
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          </>
+        )} />
       </Router>
     );
   }
