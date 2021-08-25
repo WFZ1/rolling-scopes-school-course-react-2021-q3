4856bdcd0db5889e71aae9c92ad0badf0a8f366f
WFZ1 <pavelskumenko@gmail.com>
2021-08-25 20:58:04 +0300

    feat: add testing the details page

---
+++ react-testing/test/details-page.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
+import DetailsPage from '../src/components/details-page/details-page';
+
+describe('Details Page', () => {
+  test('Should render page', () => {
+    const {getByRole} = render(
+      <Provider store={store}>
+        <DetailsPage />
+      </Provider>
+    );
+
+    expect(getByRole('heading', { name: 'Details Page' })).toBeTruthy();
+  });
+});
