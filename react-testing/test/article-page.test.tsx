29927a638fb7c7685428d57871b7fb461a37515a
WFZ1 <pavelskumenko@gmail.com>
2021-08-25 21:28:59 +0300

    feat: add testing the article page

---
+++ react-testing/test/article-page.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
+import ArticlePage from '../src/components/article-page/article-page';
+
+describe('Article Page', () => {
+  test('Should render page', async () => {
+    render(
+      <Provider store={store}>
+        <ArticlePage />
+      </Provider>
+    );
+
+    expect(document.body.classList.contains('article-page')).toBeTruthy();
+  });
+});
